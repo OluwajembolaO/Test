8e6ad305927f5ea934e55a9d899c2136d7de2270
Oluwajembola Orioke <oluwajembola.orioke@bam-academy.com>
2023-08-17 13:34:17 -0400

    push

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from datetime import datetime
 from pwd import *
 import openai
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 #creates Flask instance
 app = Flask(__name__)
 openai.api_key = apiKey
+messages = []
 #DataBase
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-
 #Create a model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -48,42 +49,47 @@ def contacts():
 def about():
     return render_template('about.html')
 
+
+
+# ... (other imports and code)
+
 @app.route("/chatBot", methods=("GET", "POST"))
 def chatBot():
+    global messages
+
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("chatBot", result=response.choices[0].text))
+        user_input = request.form["user_input"]
+        messages.append({
+            "sender": "user",
+            "content": "Pretend your name is Bo. Bo the Bunny. You are a bunny. Your role today is to be a nice friend who assists others in need of mental help! You will speak in a respective language. You will be able to help others in need of mental help. You will speak in a respective tone.",
+            "text": user_input
+        })
 
-    result = request.args.get("result")
-    return render_template("chatBot.html", result=result)
+        conversation = "\n".join([f"User: {msg['text']}" if msg['sender'] == 'user' else f"Bo: {msg['text']}" for msg in messages])
+        ai_response = get_ai_response(conversation)
 
+        messages.append({
+            "sender": "bot",
+            "content": user_input,
+            "text": ai_response
+        })
+        
+        return redirect(url_for("chatBot"))
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return render_template("chatBot.html", messages=messages)
 
-    Animal: Cat
-    Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-    Animal: Dog
-    Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-    Animal: {}
-    Names:""".format(
-        animal.capitalize()
+def get_ai_response(conversation):
+    persona = "Hi, I'm Bo, your helpful friend. Your full name is Bo the Bunny. Your role today is to be a nice friend who assists others in need of mental help! You will speak in a respective language. You will be able to help others in need of mental help. You will speak in a respective tone.You will always remember the users name when they tell you their name."
+    prompt = f"{persona}\n{conversation}"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.6,
+        max_tokens=150,
     )
+    return response.choices[0].text
 
-@app.route('/signIn', methods=['GET', 'POST'])
-def signIn():
-    if request.method == "POST":
-        # Perform login authentication here, for simplicity, let's just assume it's successful
-        username = request.form.get('username')
-        session['username'] = username  # Store the username in the session
-        return redirect(url_for('index'))
-    return render_template('signIn.html')
-
+# ... (other routes and code)
 
 @app.route('/logout')
 def logout():
@@ -96,12 +102,49 @@ def signUp():
         userName = request.form.get('username')
         pwd = request.form.get('password')
         email = request.form.get('email')
-        if len(pwd) < 5:
+        
+        emailExsist = User.query.filter_by(email = email).first()
+        userNameExsist = User.query.filter_by(name = userName).first()
+        
+        if emailExsist:
+            error_message = f"This email ({email}) already exists."
+            return render_template('signup.html', error=error_message)
+        elif userNameExsist:
+            error_message = f"This username ({userName}) already exists."
+            return render_template('signup.html', error=error_message)
+        elif len(pwd) < 5:
             error_message = "Your password is too short. It must be at least 8 characters long."
             return render_template('signup.html', error=error_message)
+        else:
+            newUser = User(name = userName, email = email, password = generate_password_hash(pwd, method='sha256'))
+            db.session.add(newUser)
+            db.session.commit()
+                  
         print(userName, pwd, email)
         return redirect(url_for('index'))
     return render_template('signUp.html')
+
+@app.route('/signIn', methods=['GET', 'POST'])
+def signIn():
+    if request.method == "POST":
+        userName = request.form.get('username')
+        pwd = request.form.get('password')
+        print(userName, pwd)
+        user = User.query.filter_by(name=userName).first()
+        if user is None:
+            error_message = f"This username ({userName}) doesn't exist. Please try again"
+            return render_template('signIn.html', error=error_message)
+        else:
+            print("Username Exists")
+            if check_password_hash(user.password, pwd):
+                session['username'] = user.name  # Store the username in the session
+                return redirect(url_for('index'))
+            else:
+                print("Incorrect password")
+                error_message = "Incorrect password. Please try again"
+                return render_template('signIn.html', error=error_message)
+    return render_template('signIn.html')
+
 
 if __name__ == '__main__':
     with app.app_context():
